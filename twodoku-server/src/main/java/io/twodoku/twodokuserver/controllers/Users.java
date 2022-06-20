ddf02e0ac04c3541906929b4a2251b4373f61052
Justin <jtat5912@gmail.com>
2022-06-20 01:26:08 -0700

    Added error checking for getAccount path. getAccount path is finished

--- twodoku-server/src/main/java/io/twodoku/twodokuserver/controllers/Users.java
+++ twodoku-server/src/main/java/io/twodoku/twodokuserver/controllers/Users.java
@@ -4,15 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.twodoku.twodokuserver.models.User;
 import io.twodoku.twodokuserver.repository.UserInterface;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,22 +27,21 @@ public class Users {
   }
 
   @GetMapping("/getAccount")
-  public ResponseEntity<User> getAccount(@RequestParam("username") String username, @RequestParam("password") String password) {
+  public ResponseEntity getAccount(@RequestParam("username") String username, @RequestParam("password") String password) {
     try{
       User user = userInterface.findByName(username);
+      if (user == null) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Username not found");
+      }
       if (user.getPassword().equals(password)) {
         System.out.println("Found password matches ");
         return new ResponseEntity<>(user, HttpStatus.OK);
       }
       System.out.println("Found passwords do not match Given: " + password + " || Expecting: " + user.getPassword());
-      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+      return ResponseEntity.status(HttpStatus.CONFLICT).body("Password did not match that account");
     } catch(Exception e) {
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    // Map<String, String> response = new HashMap<>();
-    // response.put("name", username);
-    // response.put("password", password);
-    // return response;
   }
   
 }
